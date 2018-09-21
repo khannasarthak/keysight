5db9d0bd7089561c7dd1b99b54354e78b5b83d75
Sarthak Khanna <khannasarthak.1994@gmail.com>
2018-09-21 01:26:30 -0400

    Working
    Need to do a little code refactor.

--- scripts.js
+++ scripts.js
@@ -1,28 +1,38 @@
-console.log("Connected");
-
 functions = {
-    add: function(a, b) {
-        console.log(a + b);
+    add: {
+        function(a, b) {
+            console.log(a + b);
+        },
+        color: "#5FC5FF"
     },
 
-    subtract: function(a, b, c) {
-        console.log(a - b);
+    subtract: {
+
+        function(a, b, c) {
+            console.log(a - b);
+        },
+        color: "#FE7F7F"
     },
 
-    multiply: function(a, b, c, d) {
-        console.log(a - b);
+    multiply: {
+        function(a, b, c, d) {
+            console.log(a - b);
+        },
+        color: "#95F981"
     },
 
-    negate: function(a) {
-        console.log(-a);
+    negate: {
+        function(a) {
+            console.log(-a);
+        },
+        color: "#FEE36C"
     },
 
-    square: function(a) {
-        console.log(a * a);
-    },
-
-    root: function(a) {
-        console.log(a * a);
+    square: {
+        function(a) {
+            console.log(a*a);
+        },
+        color: "#FE91EA"
     }
 
 
@@ -32,14 +42,19 @@ functions = {
 
 for (f in functions) {
     //$('#functionList').append('<div class="col-md-3 function" id="' + f + '">' + f + '</div>');
-    $('#functionList').append('<div class="btn btn-outline-primary function draggable" id="' + f + '">' + f.toUpperCase() + '</div>');
+    var color = functions[f].color;
+    style="background-color:"+color;
+    $('#functionList').append('<a class="btn function draggable" style="'+style+'" id="' + f + '">' + f.toUpperCase() + '</a>');
+
 }
+
+
 
 $('.draggable').draggable({
     revert: true,
     cursorAt: {
-        top: -5,
-        left: -5
+        top: 56,
+        left: 100
     }
 });
 
@@ -48,33 +63,137 @@ $("#clear").click(function() {
 });
 
 
+
+
+function getWidth(parameters) {
+
+    width = 'calc(85% /' + parameters + ')';
+
+
+    // var a = $('.params').parent().width();
+    // console.log(a)
+    return width;
+}
+
+var level = 0;
+
+function getChildren(parentParam) {
+
+    if (level > 8) {
+        alert("Hard Coded recursive depth reach, clearing screen");
+        $('#expressions').empty();
+        level = 0;
+        return;
+    }
+    console.log("---LEVEL->" + level);
+    var style;
+    // var childDiv = '<div class="params" style="'+style+'"></div>';
+    // var childDiv = '<div class="params"></div>';
+    // console.log(childDiv);
+    var cParam;
+    // console.log("1_"+cParam);
+    $('.params').droppable({
+        greedy: true,
+
+        drop: function(event, ui) {
+            level += 1;
+            // console.log("test");
+            var nestedFunctionDropped = ui.draggable.attr("id");
+            // console.log(nestedFunctionDropped);
+            
+            event.stopPropagation();
+            // console.log($(this));
+            // $(this).empty();
+            
+            cParam = functions[nestedFunctionDropped].function.length;
+            // console.log("2_"+cParam)
+            // console.log("Parameters: " + parameters);
+            var borderColor =  functions[nestedFunctionDropped].color;
+            // var border ="border: 1px "+borderColor+ " dotted;"
+            var bg="background-color:"+borderColor+";";
+            // var textColor = "color:"+borderColor
+            var nestedNameDiv = $('<div class="functionName">' + nestedFunctionDropped.toUpperCase() + '</div>');
+            // var style = 'width:' + getWidth(cParam) + ';border: 1px '+borderColor+ ' dotted;'
+            var style = 'width:' + getWidth(cParam) + ';'+ bg;
+            console.log("---LEVEL->" + level + '--' + style);
+            var childDiv = '<div class="params" style="' + style + '"></div>';
+            // console.log()
+            $(this).html(nestedNameDiv);
+            for (var i = 0; i < cParam; i++) {
+                // console.log("in loop")
+                $(this).append(childDiv);
+                // $(this).append()
+
+                getChildren(cParam);
+            }
+
+        }
+
+        // parameters = functions[func].length
+
+
+    });
+    // childWidth = getWidth(cParam);
+    // $('.params').width(childWidth);
+
+    // var childWidth = getWidth(parentParam);
+    //     $('.params').width(childWidth);
+    // console.log("3_"+cParam)
+    // return cParam;
+}
+
+// 
 $("#expressions").droppable({
 
     drop: function(event, ui) {
+        // level+=1;
         $('#expressions').html('');
+        id = event.target.id;
+
         var functionDropped = ui.draggable.attr("id");
-        console.log("DROOPPED! " + functionDropped);
-        $('#expressions').prepend('<div class="functionName"><p>' + functionDropped.toUpperCase() + '</p></div>');
+
+        // console.log("DROOPPED! " + functionDropped);
+        
+        // var childDiv = '<div class="params"></div>';
+        // var nchildDiv = $('<div class="params"></div>');
+        var childParam;
+        // console.log("4_"+childParam)
+
+
+        $
         // console.log(functions[draggableId](5,7));
-        parameters = functions[functionDropped].length
-        console.log("Parameters: " + parameters);
+        parameters = functions[functionDropped].function.length
+        
+        var borderColor =  functions[functionDropped].color;
+        var border ="border: 1px "+borderColor+ " dotted;"
+        // var textColor = "color:"+borderColor
+        // console.log(border);
+        var bg="background-color:"+borderColor+";";
+        var nameDiv = $('<div class="functionName">' + functionDropped.toUpperCase() + '</div>');
+        // console.log(nameDiv)
+        // var style = 'width:' + getWidth(parameters) + ';border: 1px '+borderColor+ ' dotted;'
+        var style = 'width:' + getWidth(parameters) + ';'+ bg;
+
+        console.log(style);
+        var childDiv = '<div class="params" style="' + style + '"></div>';
+        $('#expressions').prepend(nameDiv);
+        // console.log("Parameters: " + parameters);
         for (var i = 0; i < parameters; i++) {
-
             // $('#expressions').append('<div class="params" style="min-width=' + 100 / parameters + '%"></div>');
-            $('#expressions').append('<div class="params"></div>');
+
+            $('#expressions').append(childDiv);
+            getChildren(parameters);
+            // console.log(console.log("5_"+childParam));
+
+
+
+
+
         }
-        parameterWidth = String(94 / parameters) + '%';
-        console.log(parameterWidth);
-        $('.params').width(parameterWidth);
+        // console.log(childParam);
+        // childWidth = getWidth(parameters);
+        // $('.params').width(childWidth);
 
-        // $('#output').text();
     }
-});
 
-$(document).on("click", ".params", function(event) {
-    event.stopPropagation();
-    console.log("Clicked!");
-    var copylast = $(this).parents()[0].outerHTML;
-    console.log(copylast);
-    $(this).empty().append(copylast);   
 });
